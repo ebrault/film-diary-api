3881bd41bc1da9965546161ad7c32efb72878e07
ebrault <emmet.brault@gmail.com>
2018-10-18 11:35:24 -0400

    Fixed relationship between tables
    - Users have many films
    - Added film belongs to user
    - Generated migration add user to film to associate user foreign key
    with films

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_130529) do
+ActiveRecord::Schema.define(version: 2018_10_18_152144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2018_10_18_130529) do
     t.string "director"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_films_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +43,5 @@ ActiveRecord::Schema.define(version: 2018_10_18_130529) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "films", "users"
 end
