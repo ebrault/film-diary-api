018874b593624dee10233a97ca0c6a486d708ea7
ebrault <emmet.brault@gmail.com>
2018-10-19 12:38:12 -0400

    Dummy commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :years
   # RESTful routes
   resources :examples, except: %i[new edit]
   resources :films, except: %i[new edit]
